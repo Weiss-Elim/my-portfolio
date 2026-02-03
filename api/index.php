ebb946b9468ea2245a396352c6493bbb63baf515
Sean John Duque <onyok404@gmail.com>
2026-02-03 16:52:44 +0800

    Vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,7 @@
 <?php
-// Forward Vercel requests to Laravel's index.php
+// Temporary debug mode for your my-portfolio project
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require __DIR__ . '/../public/index.php';
